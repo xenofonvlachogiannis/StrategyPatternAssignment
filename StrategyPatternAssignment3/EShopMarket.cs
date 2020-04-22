1e29ff3d40124fa5b688ec76713a1fec6f030285
Xenofontas Vlachogiannis <xenofonvlachogiannis@gmail.com>
2020-04-22 14:53:46 +0300

    Add tryparse for checking inputs

--- StrategyPatternAssignment3/EShopMarket.cs
+++ StrategyPatternAssignment3/EShopMarket.cs
@@ -7,27 +7,40 @@ namespace StrategyPatternAssignment3
     {
         public static void EshopChoose()
         {
+            int choice;
             Console.WriteLine("Welcome to my T-Shirt e-shop.");
             Console.WriteLine();
             Console.WriteLine("Please choose your T-shirt color. \n1) Red, 2) Orange, 3) Yellow, 4) Green, 5) Blue, 6) Indigo, 7) Violet");
-            var tshirtColor = int.Parse(Console.ReadLine().Trim());
+            while (!int.TryParse(Console.ReadLine().Trim(), out choice))
+            {
+                Console.WriteLine("Wrong input try again");
+            }
             Console.WriteLine();
-            var color = ChooseColor(tshirtColor);
+            var color = ChooseColor(choice);
 
             Console.WriteLine("Please choose your T-shirt size. \n1) XS, 2) S, 3) M, 4) L, 5) XL, 6) XXL, 7) XXXL");
-            var tshirtSize = int.Parse(Console.ReadLine().Trim());
+            while (!int.TryParse(Console.ReadLine().Trim(), out choice))
+            {
+                Console.WriteLine("Wrong input try again");
+            }
             Console.WriteLine();
-            var size = ChooseSize(tshirtSize);
+            var size = ChooseSize(choice);
 
             Console.WriteLine("Please choose your T-shirt fabric. \n1) Wool, 2) Cotton, 3) Polyester, 4) Rayon, 5) Linen, 6) Cashmere, 7) Silk");
-            var tshirtFabric = int.Parse(Console.ReadLine().Trim());
+            while (!int.TryParse(Console.ReadLine().Trim(), out choice))
+            {
+                Console.WriteLine("Wrong input try again");
+            }
             Console.WriteLine();
-            var fabric = ChooseFabric(tshirtFabric);
+            var fabric = ChooseFabric(choice);
 
             Console.Write("How would you like to pay? 1) Credit Card, 2) Bank Transfer, 3) Cash : ");
-            var paymentType = int.Parse(Console.ReadLine().Trim());
+            while (!int.TryParse(Console.ReadLine().Trim(), out choice))
+            {
+                Console.WriteLine("Wrong input try again");
+            }
             Console.WriteLine();
-            var paymentMethod = ChoosePaymentMethod(paymentType);
+            var paymentMethod = ChoosePaymentMethod(choice);
             var eshop = new Eshop();
 
             var variations = new List<Variation>() { new ColorVariation(), new FabricVariation(), new SizeVariation() };
@@ -116,16 +129,16 @@ namespace StrategyPatternAssignment3
             switch (paymentMethod)
             {
                 case 1:
-                    return  new CreditCard();
+                    return new CreditCard();
 
                 case 2:
-                    return  new BankTransfer();
+                    return new BankTransfer();
 
                 case 3:
-                    return  new Cash();
+                    return new Cash();
 
                 default:
-                    return  new CreditCard();
+                    return new CreditCard();
 
             }
         }
